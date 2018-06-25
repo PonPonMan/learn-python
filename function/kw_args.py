f73d55cfb3e3d9c2de3c7c35bb6420a170d1d592
PonPonMan <412611687@qq.com>
2018-06-25 15:06:56 +0800

    2018.6.25

--- function/kw_args.py
+++ function/kw_args.py
@@ -30,3 +30,18 @@ else:
         print('测试成功!')
     except TypeError:
         print('测试失败!')
+
+
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+
+
+f1(1, 2)
+f1(1, 2, c=3)
+f1(1, 2, 3, 'a', 'b')
+f1(1, 2, 3, 'a', 'b', x=99)
+f2(1, 2, d=99, ext=None)
