254d1ce8c66321bc13cb1aebb368f67bb3c51743
PonPonMan <412611687@qq.com>
2018-06-26 15:05:51 +0800

    2018.6.26

--- advance/do_iter.py
+++ advance/do_iter.py
@@ -38,3 +38,18 @@ elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
     print('测试失败!')
 else:
     print('测试成功!')
+
+# 凡是可作用于for循环的对象都是Iterable类型
+# 凡是可作用于next()函数的对象都是Iterator类型，它们表示一个惰性计算的序列
+# 集合数据类型如list、dict、str等是Iterable但不是Iterator，不过可以通过iter()函数获得一个Iterator对象
+# 把list、dict、str等Iterable变成Iterator可以使用iter()函数
+
+for x in [1, 2, 3, 4, 5]:
+    print(x)
+
+it = iter([1, 2, 3, 4, 5])
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        break
